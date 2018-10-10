d79bfb6234d4422a73c2212f66bdddc2dc1767cd
Vinícius Negrão <aeyos.dev@gmail.com>
2018-10-10 15:22:04 -0300

    [ * ][ ColorSelector ] Added pretty colors

--- src/Game/ColorSelector.js
+++ src/Game/ColorSelector.js
@@ -1,19 +1,36 @@
+import { TextureLoader } from "three";
 import { Animation, EmptyObject, Sprite } from "../Engine";
 
 class ColorSelector extends EmptyObject {
   constructor(args) {
     super();
-    for (let i = 0; i < 18; i++) {
-      const spr = new Sprite({
-        texture:
-          "https://uploads.codesandbox.io/uploads/user/e3b4aea8-bdf2-48b5-8c19-b1bfeb3a0c27/vzRc-ball.png"
-      });
 
-      this.add(spr);
+    const colors = 25;
 
-      spr.scale.x = 64;
-      spr.scale.y = 64;
-    }
+    const canvas = document.createElement("canvas");
+    canvas.width = 64;
+    canvas.height = 64;
+
+    const ctx = canvas.getContext("2d");
+    const tex = new TextureLoader().load(
+      "https://uploads.codesandbox.io/uploads/user/e3b4aea8-bdf2-48b5-8c19-b1bfeb3a0c27/vzRc-ball.png",
+      () => {
+        console.log(tex.image);
+        for (let i = 0; i < colors; i++) {
+          ctx.clearRect(0, 0, 64, 64);
+          ctx.drawImage(tex.image, 0, 0);
+          ctx.fillStyle = `hsl(${i * Math.round(360 / colors)}, 90%, 66%)`;
+          ctx.beginPath();
+          ctx.arc(32, 32, 25, 0, Math.PI * 2);
+          ctx.fill();
+          const spr = new Sprite({
+            texture: canvas.toDataURL()
+          });
+
+          this.add(spr);
+        }
+      }
+    );
 
     this.attach(new Animation());
     this.animation.register(
@@ -29,15 +46,16 @@ class ColorSelector extends EmptyObject {
               anim.progress * Math.PI * 2 +
                 (i / this.children.length) * Math.PI * 2
             ) *
-              (250 * anim.progress)
+              (300 * anim.progress)
           );
           c.position.y = Math.round(
             Math.cos(
               anim.progress * Math.PI * 2 +
                 (i / this.children.length) * Math.PI * 2
             ) *
-              (250 * anim.progress)
+              (300 * anim.progress)
           );
+          c.scale.x = c.scale.y = 64 * anim.progress;
         });
       }
     );
